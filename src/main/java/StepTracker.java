42f2080042e7420930433bdc63862161933b442b
machinegunbryce <brchan1@s.sfusd.edu>
2025-04-25 23:52:18 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,32 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+private int active,steps,numDays,minimum;
+
+ public StepTracker(int d) {
+ active = 0;
+  minimum = d;
+  numDays = 0;
+  steps = 0;
+ }
+
+ public void addDailySteps(int b) {
+steps += b;
+numDays +=1;
+if(b >= minimum)
+ active++;
+
+ }
+
+ public int activeDays() {
+return active;
+ }
+
+ public double averageSteps() {
+ if(numDays==0)
+  return 0;
+else
+  return (double)steps/numDays;
+ }
+ 
 } 
